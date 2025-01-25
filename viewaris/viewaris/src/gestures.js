cbda67123216c93db50a6a249f75ac47c06867ed
zach-planet-nine <zach@planetnine.app>
2025-01-24 23:07:00 -0800

    mvp baby, let's go

---
+++ viewaris/viewaris/src/gestures.js
@@ -0,0 +1,62 @@
+const swipeThresholds = {
+  minX: 30,
+  maxX: 30,
+  minY: 50,
+  maxY: 60
+};
+
+const gestures = {
+  addSwipeGestureListener: (element, callback) => {
+    const swipeWatcher = {};
+
+    element.addEventListener('touchstart', (event) => {
+      const touch = event.touches[0];
+      swipeWatcher.startX = touch.screenX;
+      swipeWatcher.startY = touch.screenY;
+    });
+
+    element.addEventListener('touchmove', (event) => {
+      const touch = event.touches[0];
+      swipeWatcher.endX = touch.screenX;
+      swipeWatcher.endY = touch.screenY;
+    });
+
+    element.addEventListener('touchend', (event) => {
+      const xDiff = swipeWatcher.endX - swipeWatcher.startX;
+      const yDiff = swipeWatcher.endY - swipeWatcher.startY;
+      
+      if(Math.abs(yDiff) > swipeThresholds.minY && yDiff > 0) {
+        callback('up');
+      } else if(Math.abs(yDiff) > swipeThresholds.minY && yDiff < 0) {
+        callback('down');
+      } 
+    });
+  },
+
+  addMouseSwipeListener: (element, callback) => {
+    const swipeWatcher = {};
+
+    element.addEventListener('mousedown', (event) => {
+      swipeWatcher.startX = event.clientX;
+      swipeWatcher.startY = event.clientY;
+    });
+
+    element.addEventListener('mousemove', (event) => {
+      swipeWatcher.endX = event.clientX;
+      swipeWatcher.endY = event.clientY;
+    });
+
+    element.addEventListener('mouseup', (event) => {
+      const xDiff = swipeWatcher.endX - swipeWatcher.startX;
+      const yDiff = swipeWatcher.endY - swipeWatcher.startY;
+      
+      if(Math.abs(yDiff) > swipeThresholds.minY && yDiff > 0) {
+        callback('up');
+      } else if(Math.abs(yDiff) > swipeThresholds.minY && yDiff < 0) {
+        callback('down');
+      } 
+    });
+  },
+};
+
+export default gestures;
--- viewaris/viewaris/src/main.js
+++ viewaris/viewaris/src/main.js
@@ -2,6 +2,7 @@ console.log('heyoooo')
 
 const { invoke } = window.__TAURI__.core;
 const { create, mkdir, readTextFile, writeTextFile, BaseDirectory } = window.__TAURI__.fs;
+import gestures from './gestures.js';
 
 console.log('heereklrjljrlj');
 
@@ -54,15 +55,109 @@ console.warn(err);
 try {
 const feed = await invoke("get_feed", {uuid: doloresUser.uuid, tags: ""});
 
-const uuid = feed.videos[0].uuid;
+console.log(feed.videos);
+
+/*const uuid = feed.videos[0].uuid;
 const videoURL = `${baseURL}user/${doloresUser.uuid}/short-form/video/${uuid}`;
 
 const video = document.createElement('video');
 video.setAttribute('style', 'height: 100vh; width: auto; max-width: 100%; object-fit: contain;');
 video.setAttribute('src', videoURL);
-video.setAttribute('autoplay', true);
+video.setAttribute('autoplay', true);*/
 
-document.getElementById('container').appendChild(video);
+feed.videos.forEach(vid => {
+  const uuid = vid.uuid;
+  const videoURL = `${baseURL}user/${doloresUser.uuid}/short-form/video/${uuid}`;
+
+  const div = document.createElement('div');
+  div.classList.add('video-cell');
+
+  const video = document.createElement('video');
+  video.classList.add('video');
+  video.setAttribute('src', videoURL);
+  video.setAttribute('autoplay', false);
+//  video.setAttribute('loop', true);
+
+  div.appendChild(video);
+
+  document.getElementById('container').appendChild(div);
+});
+
+try {
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry, index) => {
+	  const vid = entry.target;
+
+	  if (entry.isIntersecting) {
+	      vid.play();
+	  } else {
+	      vid.pause();
+	  }
+      });
+  }, { threshold: 0.5 });
+
+  document.querySelectorAll('video').forEach(video => {
+console.log('video selector got ', video);
+      observer.observe(video);
+      
+console.log('should add ended event listener');
+      video.addEventListener('ended', () => {
+console.log('ended is being clled');
+        const nextVideo = video.parentElement.nextElementSibling;
+        
+        if (nextVideo) {
+	  nextVideo.scrollIntoView({
+	    behavior: 'smooth',
+	    block: 'start'
+	  });
+	}
+      });
+console.log('should have added ended event listener');
+  });
+
+  document.querySelectorAll('div').forEach(videoCell => {
+    gestures.addSwipeGestureListener(videoCell, (direction) => {
+      console.log('in callback', direction);
+      let nextVideo;
+
+      if(direction === 'up') {
+	nextVideo = videoCell.previousElementSibling;
+      } else if(direction === 'down') {
+	nextVideo = videoCell.nextElementSibling;
+      }
+
+      if (nextVideo) {
+	nextVideo.scrollIntoView({
+	  behavior: 'smooth',
+	  block: 'start'
+	});
+      }
+    });
+
+    gestures.addMouseSwipeListener(videoCell, (direction) => {
+      console.log('in callback', direction);
+      let nextVideo;
+        nextVideo = videoCell.nextElementSibling
+      if(direction === 'up') {
+	nextVideo = videoCell.previousElementSibling;
+      } else if(direction === 'down') {
+	nextVideo = videoCell.nextElementSibling;
+      }
+
+      if (nextVideo) {
+	nextVideo.scrollIntoView({
+	  behavior: 'smooth',
+	  block: 'start'
+	});
+      }
+    });
+  });
+
+} catch(err) {
+console.log('bigg ERROR');
+console.log(err);
+}
+//document.getElementById('container').appendChild(video);
 
 
 console.log(feed);
